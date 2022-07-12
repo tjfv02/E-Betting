dc9b8f3cdef27408fd80a3adaab17958d5a81326
josedangrn01 <josedanielgrn01@gmail.com>
2022-07-12 02:30:28 -0600

    subida de vista menu de partidos

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 // import Header from "./Components/Header"
 import Inicio from "./Components/Inicio"
 import NavBarLogin from "./Components/NavBarLogin"
-
+import SeleccionPartido from "./Components/SeleccionPartido"
 import Footer from "./Components/Footer"
 
 import Login from "./Components/Login"
@@ -24,11 +24,12 @@ const App = () => {
       
       <Router>
         <Routes>
-        <Route exact path='/' element={<Inicio />} />
-        <Route exact path='/register' element={<Register />} />
-        <Route exact path='/login' element={<Login />} />            
-        <Route exact path='/aboutus' element={<Nosotros />} />
-        <Route exact path='/calendar' element={<Calendario />} />    
+          <Route exact path='/' element={<Inicio />} />
+          <Route exact path='/register' element={<Register />} />
+          <Route exact path='/login' element={<Login />} />            
+          <Route exact path='/aboutus' element={<Nosotros />} />
+          <Route exact path='/calendar' element={<Calendario />} />    
+          <Route exact path='/seleccionpartido' element={<SeleccionPartido />} />
         </Routes>
       </Router>
    
